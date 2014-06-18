f87cfc007a6f1b684bb01629d5b375e8cf0344e6
mbaytas <m.a.baytas@gmail.com>
2014-06-18 16:57:17 +0200

    Clean upJekyll

--- _site/js/navhighlight.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $("#navlist li a").each(function(index) {
-    if ($(this).attr("href") == window.location.pathname)
-    $(this).parent().addClass("active");
-  });
-});
